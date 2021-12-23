7c56a987fab50de197255602f4df1dc69f8c25d5
anash17 <43125363+anash17@users.noreply.github.com>
2021-12-23 19:07:50 +0700

    add view user

---
+++ application/views/pendaftar/side.php
@@ -0,0 +1,40 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="#" class="brand-link">
+    <img src="<?php echo base_url('') ?>asset/dist/img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">Database Online</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+        <li class="nav-item">
+          <a href="<?php echo base_url(); ?>pendaftar" class="nav-link">
+            <i class="nav-icon fas fa-home"></i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="<?php echo base_url(); ?>pendaftar/detail" class="nav-link">
+            <i class="nav-icon fas fa-briefcase"></i>
+            <p>Database</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link"><i class="nav-icon fas fa-reply"></i>
+            <p>Log Out</p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
