39aa736a4bc5ed15a53f61edbb6de37ea2a4b056
anash17 <43125363+anash17@users.noreply.github.com>
2021-12-31 15:20:51 +0700

    Update User.php

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -31,7 +31,7 @@ class User extends CI_Controller {
 		if ($prin==0) {
 			$this->load->view('user/form-add');
 		} else {
-			$pilih			= 'db_'.strtolower($this->uri->segment(3));
+			$pilih		= 'db_'.strtolower($this->uri->segment(3));
 			$cari['data'] 	= $this->db->get_where($pilih, ["id_enc" => $this->uri->segment(4)])->row();
 			$this->load->view('user/form-lck',$cari);
 		}
@@ -41,14 +41,14 @@ class User extends CI_Controller {
 	{
 		//Fungsi db_mts
 		$jml = $this->db->query("SELECT * FROM db_mts")->num_rows();
-		$urut = $jml+1;
+		$urut = $jml + 1;
 
 		$data 				= $this->input->post();
 		$data['id_enc']		= md5($this->session->userdata('nik'));
-		$data['No_Reg']		= "538-".date("ymd")."-".sprintf('%03d', $urut);
+		$data['No_Reg']		= "538-" . date("ymd") . "-" . sprintf('%03d', $urut);
 		$data['nama']		= $this->session->userdata('nama');
 		$data['nik']		= $this->session->userdata('nik');
-		$data['progres'] 	= date();
+		$data['progres'] 	= date('Y-m-d H:i:s');
 		$data['editor']		= $this->session->userdata('nama');
 		$this->db->insert('db_mts', $data);
 
@@ -56,11 +56,11 @@ class User extends CI_Controller {
 		$id 			= $this->session->userdata('id');
 		$nik 			= $this->session->userdata('nik');
 		$data2['echo']	= "1";
-        $this->db->update('db_user_pendaftar', $data2, array('id' => $id));
+		$this->db->update('db_user_pendaftar', $data2, array('id' => $id));
 
-        //==============================================================================
-        $this->session->set_flashdata('pesan', "{icon: 'success', title: 'Alhamdulillah',text: 'Formulir berhasil di kirim'}");
-		redirect('user/form/MTS/'.md5($nik),'refresh');
+		//==============================================================================
+		$this->session->set_flashdata('pesan', "{icon: 'success', title: 'Alhamdulillah',text: 'Formulir berhasil di kirim'}");
+		redirect('user/form/MTS/' . md5($nik), 'refresh');
 	}
 	public function save_MA()
 	{
@@ -70,10 +70,10 @@ class User extends CI_Controller {
 
 		$data 				= $this->input->post();
 		$data['id_enc']		= md5($this->session->userdata('nik'));
-		$data['No_Reg']		= "510-" . date("ymd") . "-" . sprintf('%03d', $urut);
+		$data['No_Reg']		= "538-" . date("ymd") . "-" . sprintf('%03d', $urut);
 		$data['nama']		= $this->session->userdata('nama');
 		$data['nik']		= $this->session->userdata('nik');
-		$data['progres'] 	= date();
+		$data['progres'] 	= date('Y-m-d H:i:s');
 		$data['editor']		= $this->session->userdata('nama');
 		$this->db->insert('db_ma', $data);
 
@@ -86,6 +86,56 @@ class User extends CI_Controller {
 		//==============================================================================
 		$this->session->set_flashdata('pesan', "{icon: 'success', title: 'Alhamdulillah',text: 'Formulir berhasil di kirim'}");
 		redirect('user/form/MA/' . md5($nik), 'refresh');
+	}
+	public function save_SMK()
+	{
+		//Fungsi db_smk
+		$jml = $this->db->query("SELECT * FROM db_smk")->num_rows();
+		$urut = $jml + 1;
+
+		$data 				= $this->input->post();
+		$data['id_enc']		= md5($this->session->userdata('nik'));
+		$data['No_Reg']		= "265-" . date("ymd") . "-" . sprintf('%03d', $urut);
+		$data['nama']		= $this->session->userdata('nama');
+		$data['nik']		= $this->session->userdata('nik');
+		$data['progres'] 	= date('Y-m-d H:i:s');
+		$data['editor']		= $this->session->userdata('nama');
+		$this->db->insert('db_smk', $data);
+
+		//Fungsi db_user_pengguna
+		$id 			= $this->session->userdata('id');
+		$nik 			= $this->session->userdata('nik');
+		$data2['echo']	= "1";
+		$this->db->update('db_user_pendaftar', $data2, array('id' => $id));
+
+		//==============================================================================
+		$this->session->set_flashdata('pesan', "{icon: 'success', title: 'Alhamdulillah',text: 'Formulir berhasil di kirim'}");
+		redirect('user/form/SMK/' . md5($nik), 'refresh');
+	}
+	public function save_SMP()
+	{
+		//Fungsi db_smp
+		$jml = $this->db->query("SELECT * FROM db_smp")->num_rows();
+		$urut = $jml + 1;
+
+		$data 				= $this->input->post();
+		$data['id_enc']		= md5($this->session->userdata('nik'));
+		$data['No_Reg']		= "209-" . date("ymd") . "-" . sprintf('%03d', $urut);
+		$data['nama']		= $this->session->userdata('nama');
+		$data['nik']		= $this->session->userdata('nik');
+		$data['progres'] 	= date('Y-m-d H:i:s');
+		$data['editor']		= $this->session->userdata('nama');
+		$this->db->insert('db_smp', $data);
+
+		//Fungsi db_user_pengguna
+		$id 			= $this->session->userdata('id');
+		$nik 			= $this->session->userdata('nik');
+		$data2['echo']	= "1";
+		$this->db->update('db_user_pendaftar', $data2, array('id' => $id));
+
+		//==============================================================================
+		$this->session->set_flashdata('pesan', "{icon: 'success', title: 'Alhamdulillah',text: 'Formulir berhasil di kirim'}");
+		redirect('user/form/SMP/' . md5($nik), 'refresh');
 	}
 
 	public function cetak($form,$param,$id)
