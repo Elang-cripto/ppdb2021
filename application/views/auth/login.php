af2ee68370f26719d46e99c98c3a9b9006508704
anash17 <43125363+anash17@users.noreply.github.com>
2022-01-01 23:46:11 +0700

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -13,7 +13,7 @@
 
 <body>
   <!-- partial:index.partial.html -->
-  <div class="scroll-down">SCROLL DOWN
+  <div class="scroll-down">SCROLL KEBAWAH
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
       <path d="M16 3C8.832031 3 3 8.832031 3 16s5.832031 13 13 13 13-5.832031 13-13S23.167969 3 16 3zm0 2c6.085938 0 11 4.914063 11 11 0 6.085938-4.914062 11-11 11-6.085937 0-11-4.914062-11-11C5 9.914063 9.914063 5 16 5zm-1 4v10.28125l-4-4-1.40625 1.4375L16 23.125l6.40625-6.40625L21 15.28125l-4 4V9z" />
     </svg>
